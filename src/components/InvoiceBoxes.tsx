ec522d5e8d9661d5b8e2da3236a13e4bc3e561f1
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-05-22 18:44:14 +0400

    fix: conflict

--- src/components/InvoiceBoxes.tsx
+++ src/components/InvoiceBoxes.tsx
@@ -7,16 +7,16 @@ import { Link } from 'react-router-dom';
 import { AppContext } from '../App';
 function formatDate(dateString: string | number | Date) {
   const date = new Date(dateString);
-  return date.toLocaleDateString("en-GB", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
+  return date.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
   });
 }
 
 const itemVariants = {
   hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 },
+  visible: { opacity: 1, y: 0 }
 };
 
 function InvoiceBoxes() {
@@ -25,13 +25,13 @@ function InvoiceBoxes() {
   const filterInvoice = () => {
     switch (filterClick) {
       case "pending":
-        return data.filter(item => item.status === "pending"); 
+        return appData.filter(item => item.status === "pending"); 
       case "paid":
-        return data.filter(item => item.status === "paid");
+        return appData.filter(item => item.status === "paid");
       case "draft":
-        return data.filter(item => item.status === "draft");
+        return appData.filter(item => item.status === "draft");
       case "all": 
-        return data;
+        return appData;
       default:
         return appData;
     }
@@ -71,51 +71,24 @@ function InvoiceBoxes() {
             </span>}
             </div>
 
-              <div className="flex justify-between items-end flex-col gap-[1.68rem] md:items-center md:justify-center md:flex-row">
-                {!isMobile ? (
-                  <span className="font-[0.8125rem] text-[#858BB2] text-[0.8125rem] tracking-[-0.00625rem]">
-                    {invoice.clientName}
-                  </span>
-                ) : (
-                  <span className="font-bold">
-                    £ {invoice.total.toFixed(2)}
-                  </span>
-                )}
-                <div
-                  className={`${
-                    invoice.status === "paid"
-                      ? "bg-[#33D69F]"
-                      : invoice.status === "pending"
-                      ? "bg-[#FF8F00]"
-                      : "bg-[#373B53]"
-                  } bg-opacity-10 flex justify-center items-center gap-2 min-w-[6.5rem] max-w-[7.5rem] rounded-[0.375rem] pt-[0.88rem] pb-[0.69rem]`}
-                >
-                  <div
-                    className={`${
-                      invoice.status === "paid"
-                        ? "bg-[#33D69F]"
-                        : invoice.status === "pending"
-                        ? "bg-[#FF8F00]"
-                        : "bg-[black]"
-                    } w-[0.5rem] h-[0.5rem] rounded-lg`}
-                  ></div>
-                  <span
-                    className={`${
-                      invoice.status === "paid"
-                        ? "text-[#33D69F]"
-                        : invoice.status === "pending"
-                        ? "text-[#FF8F00]"
-                        : "text-[#373B53]"
-                    } font-bold`}
-                  >
-                    {invoice.status.split("")[0].toUpperCase() +
-                      invoice.status.slice(1)}
-                  </span>
-                </div>
-                {isMobile && <img src={ArrowRight} alt="" />}
+            <div className='flex justify-between items-end flex-col gap-[1.68rem] md:flex-row md:items-center md:justify-center md:flex-row'>
+             {!isMobile 
+                ?
+              <span className='font-[0.8125rem] text-[#858BB2] text-[0.8125rem] tracking-[-0.00625rem]'>
+                {invoice.clientName}
+              </span>
+               : 
+              <span className='font-bold'>£ {invoice.total.toFixed(2)}</span>}
+              <div className={`${invoice.status === "paid" ? 'bg-[#33D69F]' : invoice.status === "pending" ? 'bg-[#FF8F00]' : 'bg-[#373B53]'} bg-opacity-10 flex justify-center items-center gap-2 min-w-[6.5rem] max-w-[7.5rem] rounded-[0.375rem] pt-[0.88rem] pb-[0.69rem]`}>
+                <div className={`${invoice.status === "paid" ? "bg-[#33D69F]" : invoice.status === "pending" ? 'bg-[#FF8F00]' : 'bg-[black]'} w-[0.5rem] h-[0.5rem] rounded-lg`}></div>
+                <span className={`${invoice.status === "paid" ? "text-[#33D69F]" : invoice.status === "pending" ? "text-[#FF8F00]" : "text-[#373B53]"} font-bold`}>
+                  {invoice.status.split("")[0].toUpperCase() + invoice.status.slice(1)}
+                </span>
               </div>
-            </motion.div>
-          </Link>
+              {isMobile && <img src={ArrowRight} alt="" />}
+            </div>
+          </motion.div>
+           </Link>
         ))}
       </div>
     </div>
